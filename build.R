f3c6128ff47421a8dc0a4e70bd4094fdafcb41aa
Selby <david@selbys.org.uk>
2017-06-17 14:59:28 +0100

    Fix issue with slashes

--- build.R
+++ build.R
@@ -1,6 +1,6 @@
 local({
   baseurl <- servr:::jekyll_config('.', 'baseurl', default = '/')
-  knitr::opts_knit$set(base.url = baseurl)
+  knitr::opts_knit$set(base.url = paste0(baseurl, '/'))
   # fall back on 'kramdown' if markdown engine is not specified
   markdown = servr:::jekyll_config('.', 'markdown', 'kramdown')
   # see if we need to use the Jekyll render in knitr
@@ -12,8 +12,8 @@ local({
   a = commandArgs(TRUE)
   d = gsub('^_|[.][a-zA-Z]+$', '', a[1])
   knitr::opts_chunk$set(
-    fig.path   = sprintf('/figure/%s/', d),
-    cache.path = sprintf('/cache/%s/', d)
+    fig.path   = sprintf('figure/%s/', d),
+    cache.path = sprintf('cache/%s/', d)
   )
   
   knitr::opts_knit$set(width = 70)
